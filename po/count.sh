77e7ba0f1b2f298f64502572547b97c52e390de3
andrew <andrew@15aabe80-92ba-4a94-8e3d-43d3d4e8a004>
2007-08-11 17:11:01 +0000

    updated readme

--- po/count.sh
+++ po/count.sh
@@ -9,6 +9,7 @@ do
   cat temp | awk '{ str=substr($0, match($0, /([0-9]+) translated/), RLENGTH); if(str)print str;else print 0; }' | awk '{ print $1 }' > `basename $NAME .po`.percent
   cat temp | awk '{ str=substr($0, match($0, /([0-9]+) fuzzy/), RLENGTH); if(str)print str;else print 0; }' | awk '{ print $1 }' >> `basename $NAME .po`.percent
   cat temp | awk '{ str=substr($0, match($0, /([0-9]+) untranslated/), RLENGTH); if(str)print str;else print 0; }' | awk '{ print $1 }' >> `basename $NAME .po`.percent
+  rm `basename $NAME .po`.mo
 done
 
 rm -f temp
