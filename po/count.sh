8b8b2a6f1b288fdfda6b12f5aa3feff6bc0b8988
andrew <andrew@15aabe80-92ba-4a94-8e3d-43d3d4e8a004>
2014-11-24 04:09:38 +0000

    Added 64 translations generated by the OSTD ( http://littlesvr.ca/ostd ): Afrikaans, Amharic, Aragonese, Assamese, Azerbaijani, Belarusian, Bengali, Breton, Welsh, Dzongkha, Persian, Fulah, Faroese, Western, Irish, Gujarati, Hindi, Haitian, Armenian, Interlingua, Indonesian, Icelandic, Georgian, Kazakh, Centra, Korean, Kurdish, Kirghiz, Latin, Luxembourgish, Ganda, Lithuanian, Malagasy, Macedonian, Malayalam, Mongolian, Marathi, Malay, Burmese, Nepali, Occitan, Oriya, Panjabi, Pushto, Romanian, Kinyarwanda, Sinhala, Tamil, Telugu, Thai, Turkmen, Tagalog, Tatar, Uighur, Ukrainian, Urdu, Uzbek, Uzbek (Cyrillic), Vietnamese, Walloon, Wolof, Xhosa, Yiddish, and Zulu

--- po/count.sh
+++ po/count.sh
@@ -5,7 +5,7 @@ export LANG=en_CA
 for NAME in *.po
 do
   echo $NAME
-  msgfmt -c -v $NAME -o `basename $NAME .po`.mo 2> temp
+  msgfmt -v $NAME -o `basename $NAME .po`.mo 2> temp
   cat temp | awk '{ str=substr($0, match($0, /([0-9]+) translated/), RLENGTH); if(str)print str;else print 0; }' | awk '{ print $1 }' > `basename $NAME .po`.percent
   cat temp | awk '{ str=substr($0, match($0, /([0-9]+) fuzzy/), RLENGTH); if(str)print str;else print 0; }' | awk '{ print $1 }' >> `basename $NAME .po`.percent
   cat temp | awk '{ str=substr($0, match($0, /([0-9]+) untranslated/), RLENGTH); if(str)print str;else print 0; }' | awk '{ print $1 }' >> `basename $NAME .po`.percent
